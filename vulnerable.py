e798d7b7014fbe4e1f8d587154c1f8c0527b88e0
rishisqsh <143689658+rishisqsh@users.noreply.github.com>
2024-05-07 13:04:25 +0530

    Update vulnerable.py

--- vulnerable.py
+++ vulnerable.py
@@ -60,4 +60,22 @@ def main():
     arbitrary_file_write("vulnerable_file.txt", "This file is vulnerable to arbitrary file write!")
 
 if __name__ == "__main__":
+from aws_cdk import aws_ec2 as ec2
+
+instance = ec2.Instance(
+    self,
+    "my_instance",
+    instance_type=nano_t2,
+    machine_image=ec2.MachineImage.latest_amazon_linux(),
+    vpc=vpc
+)
+
+instance.connections.allow_from(
+    ec2.Peer.any_ipv4(), # Noncompliant
+    ec2.Port.tcp(22),
+    description="Allows SSH from all IPv4"
+)
+instance.connections.allow_from_any_ipv4( # Noncompliant
+    ec2.Port.tcp(3389),
+    description="Allows Terminal Server from all IPv4"
     main()
